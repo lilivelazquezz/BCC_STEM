fd0079970643d674b3c582db7a6644dcce3c7c96
LILIANA VELAZQUEZ <lilivelazquezz@gmail.com>
2026-06-02 10:55:28 -0700

    Unit one update

---
+++ BCC_JAVA/src/UnitOne.java
@@ -0,0 +1,23 @@
+public class UnitOne {
+    UnitOne() {
+        /*
+        Integrated Development Environment (IDE) provides tools for writing, compiling, and running code
+        compiler: checks for errors, and the typed code makes sense before it can run
+        run: follows the instructions typed in, executes commands
+        print: displays the information inside the quotes on the screen
+        System.out: output stream, a system component for displaying information
+        print(): method to output text
+        Java helps debugging by sending error messages
+         */
+        System.out.println("Hello World!"); //Prints in a new line
+        System.out.print("Unit "); //Prints inline
+        System.out.print("One"); //Prints inline
+
+        /*
+        https://math.hws.edu/javanotes/c2/s4.html#basics.4
+        https://docs.oracle.com/javase/tutorial/getStarted/intro/definition.html
+        https://www.biography.com/scientist/grace-hopper
+
+         */
+    }
+}
